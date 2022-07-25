c8ca117eb797a104c3ebb303fec923fe2fb0f694
“Odedina <odedina.adewunmi@outlook.com>
2022-07-25 02:35:21 +0100

    virtual properties

--- src/models/tourModels.ts
+++ src/models/tourModels.ts
@@ -51,5 +51,13 @@ const tourSchema = new mongoose.Schema({
         select: false
     },
     startDates: [Date]
+}, {
+    toJSON: {virtuals: true},
+    toObject: {virtuals: true},
+})
+
+
+tourSchema.virtual('durationWeeks').get(function(){
+    return this.duration/7
 })
 export const Tour = mongoose.model('Tour', tourSchema)
